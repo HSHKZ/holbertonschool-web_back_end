3f8c12c119eaec2eab49f89c65fb080032b041e9
Lucas Niel <lucasolegniel@gmail.com>
2024-12-13 14:38:56 +0100

    NoSQL

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Stats about Nginx logs"""
+""" Stats about Nginx """
 
 from pymongo import MongoClient
 
 
-def nginx_stats():
-    """Fetches and prints stats about Nginx logs."""
-    client = MongoClient()
+def log_stats():
+    """prints stats about Nginx logs"""
+    client = MongoClient("mongodb://localhost:27017/")
     db = client.logs
     collection = db.nginx
 
@@ -15,19 +15,13 @@ def nginx_stats():
 
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-    pipeline = [
-        {"$group": {"_id": "$method", "count": {"$sum": 1}}},
-        {"$match": {"_id": {"$in": methods}}}
-    ]
-    method_counts = {doc["_id"]: doc["count"] for doc in collection.aggregate(pipeline)}
-
     for method in methods:
-        print(f"\tmethod {method}: {method_counts.get(method, 0)}")
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
     status_check = collection.count_documents({"method": "GET", "path": "/status"})
     print(f"{status_check} status check")
 
 
 if __name__ == "__main__":
-    nginx_stats()
+    log_stats()
